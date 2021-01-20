8a92e11b43e51e49966d4bfae50fc4e4c4a53f0f
ted3x <tedo.manvelidze@space.ge>
2021-01-20 12:08:10 +0400

    final

--- src/Components/Characters/characters.js
+++ src/Components/Characters/characters.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import Character from '../FindCharacter/character'
 
 const characterStyle = {
     display: 'flex',
@@ -20,8 +21,34 @@ class Characters extends React.Component{
         this.state ={
             characters: [],
             charactersNumber: 20,
-            charactersMaxNumber: 20
+            charactersMaxNumber: 20,
+            hiddenElements: []
           }
+    }
+
+    decreaseCharacters = () =>{
+        if(this.state.charactersNumber > 0) this.setState({charactersNumber : this.state.charactersNumber - 1})
+    }
+
+    increaseCharacters = () =>{
+        if(this.state.charactersNumber < this.state.charactersMaxNumber) this.setState({charactersNumber : this.state.charactersNumber + 1})
+    }
+
+    hideComponent = (id) =>{
+        if(this.state.hiddenElements.includes(id)) return false
+        else{
+            this.state.hiddenElements.push(id)
+            this.setState({hiddenElements : this.state.hiddenElements})
+        } 
+        console.log(this.state.hiddenElements)
+    }
+
+    drawCharacter = (character) =>{
+        if(this.state.hiddenElements.includes(character.id)){
+            console.log(character.id)
+            return null
+        } 
+        else return <Character id={character.id} character={character} component={this.hideComponent}/>
     }
     
     async componentDidMount(){
@@ -34,28 +61,17 @@ class Characters extends React.Component{
     }
 
     render(){
-        const decreaseCharacters = () =>{
-            if(this.state.charactersNumber > 0) this.setState({charactersNumber : this.state.charactersNumber - 1})
-        }
-
-        const increaseCharacters = () =>{
-            if(this.state.charactersNumber < this.state.charactersMaxNumber) this.setState({charactersNumber : this.state.charactersNumber + 1})
-        }
         return(
             <Fragment>
                 <div>
-                    <button class="decrease" onClick={decreaseCharacters} type="button">-</button>
+                    <button class="decrease" onClick={this.decreaseCharacters} type="button">-</button>
                     <input type="text" value={this.state.charactersNumber} onChange={this.state.charactersNumber}></input>
-                    <button class="increase" onClick={increaseCharacters} type="button">+</button>
+                    <button class="increase" onClick={this.increaseCharacters} type="button">+</button>
                 </div>
-                <div className="characters" style={characterStyle}>
+                <div className="characters" style={characterStyle} onChange={this.state.hiddenElements}>
                 {
                     this.state.characters.slice(0,this.state.charactersNumber).map(character => (
-                        <div className="characters-child" style={characterChildStyle}>
-                            <h3>{character.name}</h3>
-                            <img src={character.image}></img>
-                        </div>
-                        ))
+                        this.drawCharacter(character)))
                 }
                 </div>
             </Fragment>
--- src/Components/FindCharacter/character.js
+++ src/Components/FindCharacter/character.js
@@ -5,25 +5,34 @@ class Character extends React.Component{
     constructor(props){
         super(props);
         this.state ={
-            character: null
+            id: this.props.id,
+            character: this.props.charater
           }
     }
 
+    hideComponent = () =>{
+        this.props.component(this.state.id)
+    }
+
     async componentDidMount(){
-        var name = this.props.location.state.characterName;
-        if(name === 'undefined' || name == "") return false;
-        const character = await fetch("https://rickandmortyapi.com/api/character/?name=" + name);
-        const characterResponse = await character.json();
-        if(characterResponse.results !== undefined){
-            this.setState({
-                character: characterResponse.results[0]
-            })
+        this.setState({character : this.props.character, id: this.props.id})
+        if(this.props.character != null) return false
+        else{
+            var name = this.props.location.state.characterName;
+            if(name === 'undefined' || name == "") return false;
+            const character = await fetch("https://rickandmortyapi.com/api/character/?name=" + name);
+            const characterResponse = await character.json();
+            if(characterResponse.results !== undefined){
+                this.setState({
+                    character: characterResponse.results[0]
+                })
+            }
         }
     }
 
     render(){
         return(
-            this.state.character? <div className="characters-child">
+            this.state.character? <div className="characters-child" onClick={this.hideComponent}>
                 <h3>{this.state.character.name}</h3>
                 <img src={this.state.character.image}></img>
             </div> : <p>Character not found</p>
